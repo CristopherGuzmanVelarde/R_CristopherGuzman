29d72547c08aafbf721e07c51abd51ef50961f38
Cristopher Guzmán Velarde <111779127+CristopherGuzmanVelarde@users.noreply.github.com>
2023-08-28 12:32:27 -0500

    Add files via upload

---
+++ Calculadora/PruebaCalculadora.java
@@ -0,0 +1,22 @@
+package Calculadora;
+
+import calculadora01.Calculadora;
+
+public class PruebaCalculadora {
+    public static void main(String[] args) {
+        // Crear un objeto de la clase Calculadora
+        Calculadora miCalculadora = new Calculadora();
+
+        // Realizar algunas operaciones
+        double resultadoSuma = miCalculadora.sumar(5.0, 3.0);
+        double resultadoResta = miCalculadora.restar(10.0, 7.0);
+        double resultadoMultiplicacion = miCalculadora.multiplicar(4.0, 2.5);
+        double resultadoDivision = miCalculadora.dividir(15.0, 3.0);
+
+        // Imprimir resultados
+        System.out.println("Resultado Suma: " + resultadoSuma);
+        System.out.println("Resultado Resta: " + resultadoResta);
+        System.out.println("Resultado Multiplicación: " + resultadoMultiplicacion);
+        System.out.println("Resultado División: " + resultadoDivision);
+    }
+}
